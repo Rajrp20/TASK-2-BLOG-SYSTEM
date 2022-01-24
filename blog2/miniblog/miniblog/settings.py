acae9dabf14f6e3d321bc93389fd715875e42289
Rajrp20 <91689114+Rajrp20@users.noreply.github.com>
2022-01-24 23:16:24 +0530

    Update settings.py

--- blog2/miniblog/miniblog/settings.py
+++ blog2/miniblog/miniblog/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5_&iqdeg*awqicvedp!pege%%lnreg0z0@7x5@7+t2*8fa-u0y'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
